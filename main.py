27519209f362493ef8c7f90b3ead5a04ae0a77bf
kezhengzhu <kezheng.zhu14@imperial.ac.uk>
2018-11-17 12:46:13 +0000

    Addding default functions onto main.py

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import numpy as np
 import pandas as pd
 
+
+def db(st):
+    dbOn = True
+    print(st)
+
+
 def load_data(filename):
     loaded_data = pd.read_csv('California/train/'+filename+'.csv')
     return loaded_data
+
+
+def main():
+    print("placeholder")
+
+if __name__ == '__main__':
+    main()
