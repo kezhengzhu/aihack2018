14734d616b8a6de1c766b7271ce20baad1d4531a
kezhengzhu <kezheng.zhu14@imperial.ac.uk>
2018-11-17 22:27:55 +0000

    Input list .py function

---
+++ get_short_list.py
@@ -0,0 +1,147 @@
+
+
+def get_list():
+    res = [ 'B00001e1', # Unweighted population
+            'B00002e1', # Unweighted no of households
+            'B01001e1', # Total population
+            'B11001e1', # Total no of households
+            'B01001e2', # Male
+            'B01001e26', # Female
+            'B01001e3', # M age start
+            'B01001e4',
+            'B01001e5',
+            'B01001e6',
+            'B01001e7',
+            'B01001e8',
+            'B01001e9',
+            'B01001e10',
+            'B01001e11',
+            'B01001e12',
+            'B01001e13',
+            'B01001e14',
+            'B01001e15',
+            'B01001e16',
+            'B01001e17',
+            'B01001e18',
+            'B01001e19',
+            'B01001e20',
+            'B01001e21',
+            'B01001e22',
+            'B01001e23',
+            'B01001e24',
+            'B01001e25', # M age end
+            'B01001e27', # F age start
+            'B01001e28',
+            'B01001e29',
+            'B01001e30',
+            'B01001e31',
+            'B01001e32',
+            'B01001e33',
+            'B01001e34',
+            'B01001e35',
+            'B01001e36',
+            'B01001e37',
+            'B01001e38',
+            'B01001e39',
+            'B01001e40',
+            'B01001e41',
+            'B01001e42',
+            'B01001e43',
+            'B01001e44',
+            'B01001e45',
+            'B01001e46',
+            'B01001e47',
+            'B01001e48',
+            'B01001e49', # F age end
+            'C02003e3', # Total white
+            'C02003e4', # Total black
+            'C02003e5', # Total native
+            'C02003e6', # Total asian
+            'C02003e7', # Total hawaiian
+            'C02003e8', # Total others
+            'B03003e3', # Total Hispanic and Latinos
+            'B11001e2', # Total Family households
+            'B11001e7', # Total non-family households
+            'B11003e1', # Total families with children
+            'B11003e2', # Families w child married
+            'B11003e8', # Families w child not married
+            'B14007e2', # Total school enrollment
+            'B14007e3', # Nusery enrollment 
+            'B14007e4', # Kindergarten
+            'B14007e5', # Grade ..
+            'B14007e6',
+            'B14007e7',
+            'B14007e8',
+            'B14007e9',
+            'B14007e10',
+            'B14007e11',
+            'B14007e12',
+            'B14007e13',
+            'B14007e14',
+            'B14007e15',
+            'B14007e16', # Grade 12
+            'B14007e17', # College
+            'B14007e18', # Grad or prof school
+            'B14002e5', # Public: Male start
+            'B14002e8',
+            'B14002e11',
+            'B14002e14',
+            'B14002e17',
+            'B14002e20',
+            'B14002e23', # Public: Male end
+            'B14002e29', # Private: Female start
+            'B14002e32',
+            'B14002e35',
+            'B14002e38',
+            'B14002e41',
+            'B14002e44',
+            'B14002e47', # Public: Female end
+            'B14002e6', # Private: Male start
+            'B14002e9',
+            'B14002e12',
+            'B14002e15',
+            'B14002e18',
+            'B14002e21',
+            'B14002e24', # Private: Male end
+            'B14002e30', # Private: Female start
+            'B14002e33',
+            'B14002e36',
+            'B14002e39',
+            'B14002e42',
+            'B14002e45',
+            'B14002e48', # Private: Female end
+            'C16002e2', # Language: English only household
+            'C16002e4', # Limited english start
+            'C16002e7', 
+            'C16002e10', 
+            'C16002e13', # Limited english end
+            'C17002e2', # Ratio income to pov <.50
+            'C17002e3', # Ratio income to pov .50-.99
+            'C17002e4', # Ratio income to pov 1.00-1.24 
+            'C17002e5', # Ratio income to pov 1.25-1.49 
+            'C17002e6', # Ratio income to pov 1.50-1.84
+            'C17002e7', # Ratio income to pov 1.84-1.99
+            'C17002e8', # Ratio income to pov >2.00
+            'B17010e1', # Families with related children
+            'B19001e2', # Income brackets: <10k
+            'B19001e3', # Income brackets: 10k-15k
+            'B19001e4', # Income brackets: 15-20k
+            'B19001e5', # Income brackets: 20k-25k
+            'B19001e6', # Income brackets: 25k-30k
+            'B19001e7', # Income brackets: 30-35
+            'B19001e8', # Income brackets: 35-40
+            'B19001e9', # Income brackets: 40-45
+            'B19001e10', # Income brackets: 45-50
+            'B19001e11', # Income brackets: 50-60
+            'B19001e12', # Income brackets: 60-75
+            'B19001e13', # Income brackets: 75-100
+            'B19001e14', # Income brackets: 100-125
+            'B19001e15', # Income brackets: 125-150
+            'B19001e16', # Income brackets: 150-200
+            'B19001e17', # Income brackets: >200k
+            'B22010e1', # Food stamps
+            'B23025e2', # Employment status: In labor force 
+            'B23025e7' # Employment status: Not in labor force
+            ]
+
+    return res
